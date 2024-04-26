7c43c270d5bf8cdf9aefcfb755aed26c8f6255d8
Andrei Ortega <75287088+Androide47@users.noreply.github.com>
2024-04-25 19:41:40 -0600

    feat: config file

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object('config.Config')
+
     from blogr import home
     app.register_blueprint(home.bp)
 
