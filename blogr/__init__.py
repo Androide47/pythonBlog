ede94d9ad219fa813aba761baea59b11b6f30632
Andrei Ortega <75287088+Androide47@users.noreply.github.com>
2024-04-28 19:57:40 -0600

    fix: pip error

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('config.Config')
+    db.init_app()
 
     from blogr import home
     app.register_blueprint(home.bp)
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+SQLITE = "sqlite:///project.db"
+POSTGRES = "posgresql+psycopg2://postgre:root@localhost:5432/blogpost_d"
+
 class Config:
     DEBUG = True
     SECRET_KEY = 'dev'
+    SQLALCHEMY_DATABASE_URI = POSTGRES
