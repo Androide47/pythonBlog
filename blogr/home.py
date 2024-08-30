7a393978b30fb273b266439fcc3d1edb2efe4952
Andrei Ortega <75287088+Androide47@users.noreply.github.com>
2024-08-29 23:25:34 -0600

    feat: buscador

--- blogr/home.py
+++ blogr/home.py
@@ -1,19 +1,31 @@
 from flask import Blueprint, render_template, request
-from models import Post, User
+from blogr.models import Post, User
 
 bp = Blueprint('home', __name__)
 
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return user
 
-
-@bp.route('/')
+def search_post(query):
+    posts = Post.query.filter(Post.title.ilike(f'%{query}%')).all()
+    return posts
+@bp.route('/', methods=('GET', 'POST'))
 def index():
     posts = Post.query.all()
-    return render_template('index.html', posts=posts)
+
+    if request.method == 'POST':
+        query = request.form.get('query')
+        posts = search_post(query)
+        value = 'hidden'
+        return render_template('index.html', posts=posts, get_user = get_user, value = value )
+
+    return render_template('index.html', posts=posts, get_user = get_user)
 
 @bp.route('/blog/<url>')
 def blog(url):
     post = Post.query.filter_by(url=url).first()
-    return render_template('blog.html', post=post)
+    return render_template('blog.html', post=post, get_user = get_user)
 
 # @bp.route('/base')
 # def auth():
